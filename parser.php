d0bba6713c5c77ec102055ad97a77ca6baab4720
Marko-What <markoFindJob@gmail.com>
2018-01-11 14:15:48 +0000

    Update parser.php

--- parser.php
+++ parser.php
@@ -11,32 +11,32 @@ if(mysqli_connect_errno()){ //it return null or error number
 ?>
 
 
-<?php
-/* sanitize fetched data */
-
-/*$data1_dirty = $_POST['$data1'];*/
-$data1=filter_var($data1_dirty, FILTER_SANITIZE_STRING);
-
-/*$data2_dirty = $_POST['$data2'];*/
-$data2=filter_var($data2_dirty, FILTER_SANITIZE_STRING);
-
-/*$data3_dirty = $_POST['$data3'];*/
-$data3=filter_var($data3_dirty, FILTER_SANITIZE_STRING);
-
-/*$data4_dirty = $_POST['$data4'];*/
-$data4=filter_var($data4_dirty, FILTER_SANITIZE_STRING);
-
-/*$data5_dirty = $_POST['$data5'];*/
-$data5=filter_var($data5_dirty, FILTER_SANITIZE_INTEGER);
-
-/*$data6_dirty = $_POST['$data6'];*/
-$data6=filter_var($data6_dirty, FILTER_SANITIZE_STRING);
-?>
 
 <?php 
 	
 	function databaseInsert($data1, $data2, $data3, $data4, $data5, $data6){
 
+		/* sanitize fetched data */
+
+	/*$data1_dirty = $_POST['$data1'];*/
+	$data1=filter_var($data1_dirty, FILTER_SANITIZE_STRING);
+
+	/*$data2_dirty = $_POST['$data2'];*/
+	$data2=filter_var($data2_dirty, FILTER_SANITIZE_STRING);
+
+	/*$data3_dirty = $_POST['$data3'];*/
+	$data3=filter_var($data3_dirty, FILTER_SANITIZE_STRING);
+
+	/*$data4_dirty = $_POST['$data4'];*/
+	$data4=filter_var($data4_dirty, FILTER_SANITIZE_STRING);
+
+	/*$data5_dirty = $_POST['$data5'];*/
+	$data5=filter_var($data5_dirty, FILTER_SANITIZE_INTEGER);
+
+	/*$data6_dirty = $_POST['$data6'];*/
+	$data6=filter_var($data6_dirty, FILTER_SANITIZE_STRING);
+
+		
 	$query = "INSERT INTO items (ColumnField1, ColumnField2, ColumnField3, ColumnField4, ColumnField5, ColumnField6) VALUES({$data1}, {$data2}, {$data3}, {$data4}, '{$data5}', '{$data6}')";
 		
 	
